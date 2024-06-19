7a460960485da335f61100a8ff218b014916603a
vikrantaroraa <arora.veshu@gmail.com>
2024-06-19 18:05:54 +0530

    feat: created various pages and components

---
+++ app/meals/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MealsLayout = ({ children }) => {
+  return (
+    <>
+      <h1>Meals Layout</h1>
+      {children}
+    </>
+  );
+};
+
+export default MealsLayout;
