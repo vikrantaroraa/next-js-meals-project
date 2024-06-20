5f157c32f00a88b5e232b66ec1b2d8089b164dbc
vikrantaroraa <arora.veshu@gmail.com>
2024-06-20 14:26:49 +0530

    feat: created meals database, populated the meals page with the data and added styles for various pages

--- app/meals/layout.js
+++ app/meals/layout.js
@@ -1,12 +1,7 @@
 import React from "react";
 
 const MealsLayout = ({ children }) => {
-  return (
-    <>
-      <h1>Meals Layout</h1>
-      {children}
-    </>
-  );
+  return <>{children}</>;
 };
 
 export default MealsLayout;
---
+++ components/main-header/nav-link.js
@@ -0,0 +1,21 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import styles from "./nav-link.module.css";
+
+const Navlink = ({ href, children }) => {
+  const path = usePathname();
+
+  return (
+    <Link
+      href={href}
+      className={path.startsWith(href) ? styles["active"] : undefined}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default Navlink;
