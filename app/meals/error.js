8dbec4e8a413b31c36317ab63730805fe4d58da5
vikrantaroraa <arora.veshu@gmail.com>
2024-06-27 18:32:06 +0530

    implemented: loading state for meals and also added error and not found pages

---
+++ app/meals/error.js
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>An error occured</h1>
+      <p>Failed to fetch meals data, please try again later</p>
+    </main>
+  );
+};
+
+export default Error;
